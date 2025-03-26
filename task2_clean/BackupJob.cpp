5a0ce41c375ea6d659ee0def0166d1faa71eaa86
VlAtrosh <Vlasosva99@gmail.com>
2025-03-26 13:07:29 +0300

    Add files via upload

---
+++ task2_clean/BackupJob.cpp
@@ -0,0 +1,20 @@
+﻿#include "BackupJob.h"
+
+void BackupJob::addFile(const std::string& filePath) {
+    files_.emplace_back(filePath);
+}
+
+void BackupJob::runBackup(const std::string& outputPath, bool splitMode) {
+    if (files_.empty()) {
+        throw std::logic_error("No files to backup");
+    }
+
+    if (splitMode) {
+        for (const auto& file : files_) {
+            storage_.saveSplit(file, outputPath);
+        }
+    }
+    else {
+        storage_.saveSingle(files_, outputPath + "/backup_full.bin");
+    }
+}
